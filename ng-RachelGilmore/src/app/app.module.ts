c49b3d1f10580a05522b2a239108137daf04e53c
Deusdedit Lutwama <ds@Ds-Pro.local>
2020-05-16 00:40:07 -0400

    Bootstrapping application structure

--- ng-RachelGilmore/src/app/app.module.ts
+++ ng-RachelGilmore/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './global/components/header/header.component';
+import { FooterComponent } from './global/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
