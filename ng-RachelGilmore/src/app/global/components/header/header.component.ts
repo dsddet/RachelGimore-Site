9f4ab054aa941474fae6294a2fccbe7e19d8062e
Deusdedit Lutwama <ds@Ds-Pro.local>
2020-05-19 15:54:33 -0400

    finalising responsive header

--- ng-RachelGilmore/src/app/global/components/header/header.component.ts
+++ ng-RachelGilmore/src/app/global/components/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  links:String[];
+  showElement:boolean;
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.links=['Home','Coaching','Empowering Entreprenuers', 'Earth and Soul Adventures','Podcasts'];
+    this.showElement=false;
+
+  }
+
+  toggleNavigation(){
+    this.showElement=!this.showElement;
   }
 
 }
