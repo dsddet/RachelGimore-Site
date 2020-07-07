724b19387888cfaec9d416c461fdd065ce7b2d22
Deusdedit Lutwama <ds@Ds-Pro.local>
2020-07-07 14:56:21 -0400

    +Section 4 bootstrap

--- ng-RachelGilmore/src/app/global/components/group-image-links/group-image-links.component.ts
+++ ng-RachelGilmore/src/app/global/components/group-image-links/group-image-links.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-group-image-links',
   templateUrl: './group-image-links.component.html',
-  styleUrls: ['./group-image-links.component.scss']
+  styleUrls: ['./group-image-links.component.scss'],
 })
 export class GroupImageLinksComponent implements OnInit {
-
-  constructor() { }
+  @Input() color: string;
+  cstyle:{color:string};
+  constructor() {}
 
   ngOnInit(): void {
+    this.cstyle={color:this.color};
   }
-
 }
